2decf8d1471fe14a06508a42270d6f6ad7e2007f
MahmoudTantora <mahmoud.tantora98@gmail.com>
2022-04-24 14:04:53 +0300

    add task3

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,10 @@ class MyApp extends StatelessWidget {
       home: const Scaffold(
         body: SafeArea(
           child: Center(
-            //TODO(task1): print the post coming from the api
+            //TODO(task1): print the post coming from the api (using print method)
             //TODO(task2): store the the post coming from api in local
-            //TODO(task2): get the post from local db and print the post title
-            ///TODO(task3) design Column with tow text and print the result of task1 in the first Text widget and print the result of task2 in the second Text widget
+            //TODO(task2): get the post from local db and print the post title(using print method)
+            //TODO(task3) design Column with tow text and print the result of task1 in the first Text widget and print the result of task2 in the second Text widget
             child: Text("Hello!!"),
           ),
         ),
